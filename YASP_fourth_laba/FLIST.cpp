a07cc51774a60fd94051e7a1b550f375bdda7f51
stepanCoolBoy2005 <pekshin005@gmail.com>
2025-02-27 00:30:06 +0300

    буэээ

--- YASP_fourth_laba/FLIST.cpp
+++ YASP_fourth_laba/FLIST.cpp
@@ -1,12 +1,5 @@
 #include "FLIST.h"
 
-//? “ри версии метода modify, выполн€ющей преобразование контейнера.Ќа вход
-//функции должны поступать итераторы начала и конца обрабатываемой части
-//контейнера, в качестве результата функци€ должна возвращать
-//модифицированный контейнер(не измен€€ исходный).–еализовать алгоритм
-//через : for, std::transform, std::for_each;
-
-
 int FLIST::sum()
 {
     int res{};
@@ -46,25 +39,26 @@ void FLIST::fill(std::fstream& file)
         ++ptr;
     }
 }
-
 std::forward_list<int> FLIST::modify_for(std::forward_list<int>::iterator start, std::forward_list<int>::iterator end)
 {
     std::forward_list<int> res;
     std::forward_list<int>::iterator iter = res.before_begin();
     int first = (*start) * (*start);
-
     for (std::forward_list<int>::iterator ptr = start; start != end; ++ptr)
     {
         if (ptr == end) break;
         res.insert_after(iter, sqrt((*ptr) * (*ptr) + first));
         ++iter;
     }
+
+
     return res;
 }
 
+
 std::forward_list<int> FLIST::modify_transform(std::forward_list<int>::iterator start, std::forward_list<int>::iterator end)
 {
-    std::forward_list<int> res{start, end};
+    std::forward_list<int> res{start, end};//
     int first = (*start) * (*start);
     std::transform(start, end, res.begin(), [&first](int x) {return sqrt((x) * (x)+first);});
     return res;
@@ -78,3 +72,66 @@ std::forward_list<int> FLIST::modify_foreach(std::forward_list<int>::iterator st
     std::for_each(start, end, [&res, &iter, &first](int x) { res.insert_after(iter, sqrt((x) * (x)+first)); iter++; });
     return res;
 }
+
+//std::forward_list<int> FLIST::modify_foreach(std::forward_list<int>::iterator start, std::forward_list<int>::iterator end)
+//{
+//    std::forward_list<int> res{ list.begin(), list.end() };
+//    int first = (*start) * (*start);
+//    auto res_start = res.begin();
+//    std::advance(res_start, std::distance(list.begin(), start));
+//    std::for_each(res_start, std::next(res_start, std::distance(start, end)), [&first](int& x) {
+//        x = std::sqrt(x * x + first); 
+//        });
+//
+//    return res;
+//}
+
+//std::forward_list<int> FLIST::modify_transform(std::forward_list<int>::iterator start, std::forward_list<int>::iterator end)
+//{
+//    std::forward_list<int> res{ list.begin(), list.end() };
+//    int first = (*start) * (*start);
+//    std::forward_list<int>::iterator res_start = res.begin();
+//    std::advance(res_start, std::distance(list.begin(), start));
+//    std::forward_list<int>::iterator res_end = res_start;
+//    std::advance(res_end, std::distance(start, end));
+//    std::transform(res_start, res_end, res_start, [&first](int x) {
+//        return std::sqrt(x * x + first);
+//        });
+//
+//    return res;
+//}
+
+//std::forward_list<int> FLIST::modify_for(std::forward_list<int>::iterator start, std::forward_list<int>::iterator end)
+//{
+//    std::forward_list<int> res;
+//    std::forward_list<int>::iterator iter = res.before_begin();
+//    int first = (*start) * (*start);
+//    if (start != list.begin())
+//    {
+//        for (std::forward_list<int>::iterator ptr = list.begin(); ptr != start; ++ptr)
+//        {
+//            if (ptr == start) break;
+//            res.insert_after(iter, *ptr);
+//            ++iter;
+//        }
+//    }
+//
+//    for (std::forward_list<int>::iterator ptr = start; start != end; ++ptr)
+//    {
+//        if (ptr == end) break;
+//        res.insert_after(iter, sqrt((*ptr) * (*ptr) + first));
+//        ++iter;
+//    }
+//
+//    if (end != list.end())
+//    {
+//        for (std::forward_list<int>::iterator ptr = end; ptr != list.end(); ++ptr)
+//        {
+//            if (ptr == list.end()) break;
+//            res.insert_after(iter, *ptr);
+//            ++iter;
+//        }
+//    }
+//
+//    return res;
+//}
